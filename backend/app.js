f22b2cb6b93194abcdf659607a4b1ebd94784557
Tanmay <sharnagattanmay@gmail.com>
2024-11-12 14:43:14 +0530

    errormiddleware changes

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import { dbConnection } from "./db/dbConnection.js";
 import books from "./routers/bookRoute.js";
 import userRoute from "./routers/userRoute.js";
-import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import { ErrorMiddleware } from "./middlewares/errorMiddleware.js";
 import fileUpload from "express-fileupload";
 
 const app = express();
@@ -35,6 +35,6 @@ app.use("/api/books", books);
 dbConnection();
 
 
-app.use(errorMiddleware);
+app.use(ErrorMiddleware);
 
 export default app;
--- backend/middlewares/ErrorMiddleware.js
+++ backend/middlewares/ErrorMiddleware.js
@@ -1,31 +1,30 @@
 class ErrorHandler extends Error {
-    constructor(message, statusCode) {
-      super(message);
-      this.statusCode = statusCode;
-    }
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
   }
-  
-export const errorMiddleware = (err, req, res, next) => {
+}
+
+export const ErrorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
 
+  // Handle specific error types
   if (err.code === 11000) {
-    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
-      err = new ErrorHandler(message, 400);
-  }
-  if (err.name === "JsonWebTokenError") {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  } else if (err.name === "JsonWebTokenError") {
     const message = `Json Web Token is invalid, Try again!`;
     err = new ErrorHandler(message, 400);
-  }
-  if (err.name === "TokenExpiredError") {
+  } else if (err.name === "TokenExpiredError") {
     const message = `Json Web Token is expired, Try again!`;
     err = new ErrorHandler(message, 400);
-  }
-  if (err.name === "CastError") {
-    const message = `Invalid ${err.path}`,
-      err = new ErrorHandler(message, 400);
+  } else if (err.name === "CastError") {
+    const message = `Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
   }
 
+  // Compile the error message
   const errorMessage = err.errors
     ? Object.values(err.errors)
         .map((error) => error.message)
@@ -37,5 +36,5 @@ export const errorMiddleware = (err, req, res, next) => {
     message: errorMessage,
   });
 };
-  
-  export default ErrorHandler;
+
+export default ErrorHandler;
