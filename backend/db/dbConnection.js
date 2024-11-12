295c35cf93a4040c0ef85f33a4e644b84b614238
Tanmay <sharnagattanmay@gmail.com>
2024-11-12 13:38:06 +0530

    git ignore done

--- backend/db/dbConnection.js
+++ backend/db/dbConnection.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
-    mongoose
-      .connect("mongodb://127.0.0.1:27017/VLIB")
-      .then(() => {
-        console.log("Connected to DB");
-      })
-      .catch((err) => {
-        console.log(`Error connecting to DB ${err}`);
-      });
-}
+  mongoose
+    .connect(process.env.MONGODB_URL, { dbName: "VLIB" })
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      console.log(`Error connecting to DB ${err}`);
+    });
+};
